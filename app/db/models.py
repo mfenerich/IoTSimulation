dd4878397c1bb15d891b30b4d7f0bc7ea00525eb
mfenerich <marcelfenerich1@gmail.com>
2024-11-29 19:03:11 +0100

    replace deprecated funcion on models.py

--- app/db/models.py
+++ app/db/models.py
@@ -8,7 +8,7 @@ This module defines the SQLAlchemy ORM models used for database tables, includin
 
 from sqlalchemy import Column, Float, Integer, String
 from sqlalchemy.dialects.postgresql import TIMESTAMP as PG_TIMESTAMP
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
